8d35f5324139e394377b66490b8d568a3294bb65
StefanoM98 <smartino030@gmail.com>
2023-03-28 12:52:25 +0200

    Add: img in VueJs

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Hello VueJs!"
+            message: "Hello VueJs!",
+            imgSrc: "https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/870x220_telerik-blogsce2fdf065dfe46a2b2054ccc57d2d18d.png?sfvrsn=222ff053_1",
+            imgAlt: "immagine di Vue JS"
         }
     }
 }).mount("#app");
